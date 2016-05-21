3380e85c206d9cdc1f089a34ca7170dce36af427
John Ahrens <john@john-ahrens.com>
2016-05-21 12:41:00 -0700

    Initial Commit

---
+++ AStar/AStar.h
@@ -0,0 +1,19 @@
+//
+//  AStar.h
+//  AStar
+//
+//  Created by John Ahrens on 5/21/16.
+//  Copyright © 2016 John Ahrens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AStar.
+FOUNDATION_EXPORT double AStarVersionNumber;
+
+//! Project version string for AStar.
+FOUNDATION_EXPORT const unsigned char AStarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AStar/PublicHeader.h>
+
+
